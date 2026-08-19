a1e96c2f046f515262a2796e19aaef338a98fece
Thaarani2008 <thaarusiva2008@gmail.com>
2026-08-19 07:01:26 +0530

    Add files via upload

--- negative coorelation.py
+++ negative coorelation.py
@@ -8,9 +8,9 @@ Created on Tue Aug 18 07:39:00 2026
 import numpy as np
 import matplotlib.pyplot as plt
 x=np.array([*range(1,9)])
-y=np.arange(90,50,-5)
+y=np.arange(90000,50000,-5000)
 plt.scatter(x,y,color='red')    
-plt.title('coorelation between study hours and exam scores')
-plt.xlabel('study hour')
-plt.ylabel('exam score')
+plt.title('coorelation between car age vs resale value')
+plt.xlabel('car age')
+plt.ylabel('resale value')
 plt.grid(True)
