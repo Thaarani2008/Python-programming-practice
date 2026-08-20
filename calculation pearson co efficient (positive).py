43a374139f02a8f895c85690f3c29fda9946c0f8
Thaarani2008 <thaarusiva2008@gmail.com>
2026-08-20 11:33:20 +0530

    Add files via upload

---
+++ calculation pearson co efficient (positive).py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 10:58:51 2026
+
+@author: Admin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.array([*range(1,9)])
+y=np.arange(55,95,5)
+n=len(x)
+xy=np.multiply(x,y)
+print(xy)
+sum_xy =  np.sum(xy)
+print(sum_xy)
+sumx=np.sum(x)
+sumy=np.sum(y)
+sumxxsumy=sumx*sumy
+x_square=np.square(x)
+y_square=np.square(y)
+r=((n*sum_xy)-(sumx)*(sumy))/np.sqrt((n*np.sum(x_square)-(np.square(sumx)))*(n*np.sum(y_square)-(np.square(sumy))))
+
+print(r)
